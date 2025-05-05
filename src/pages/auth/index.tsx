60a79b5a8dade45ceb45d930d174ab698709a91a
Yusuf <easyclick06@gmail.com>
2025-05-05 17:59:37 +0100

    Hide signup  text on the signin page

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -3,7 +3,11 @@ import { SignIn } from '@clerk/react-router';
 export default function Auth() {
   return (
     <main className="flex justify-center items-center h-screen">
-      <SignIn fallbackRedirectUrl="/" withSignUp={false} />
+      <SignIn
+        fallbackRedirectUrl="/"
+        withSignUp={false}
+        appearance={{ elements: { footerAction: '!hidden' } }}
+      />
     </main>
   );
 }
