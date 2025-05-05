ba1420727bcd7e0611d63bf0475e4edf4cdb3f6a
Yusuf <easyclick06@gmail.com>
2025-05-05 17:12:38 +0100

    * Added clerk-router and clerk-react package for authentication
    * Added authentication page and route

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from '@clerk/react-router';
+
+export default function Auth() {
+  return (
+    <main className="flex justify-center items-center h-screen">
+      <SignIn fallbackRedirectUrl="/" withSignUp={false} />
+    </main>
+  );
+}
