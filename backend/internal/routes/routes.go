ee15e08b8079b9c21816c5eabbc1226d698a2348
Yusuf <easyclick06@gmail.com>
2025-05-18 12:39:30 +0100

    * Setup mock url service, handler and routes (MVC)

---
+++ backend/internal/routes/routes.go
@@ -0,0 +1,28 @@
+package routes
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/deveasyclick/tilvio/internal/db"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/httprate"
+)
+
+func Register(db db.Service) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
+	// Enable rate limiter of 100 requests per minute per IP
+	r.Use(httprate.LimitByIP(100, 1*time.Minute))
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("alive"))
+	})
+
+	registerURLRoutes(r, db.DB)
+
+	return r
+}
