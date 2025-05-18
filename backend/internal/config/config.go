cc1e13db7225df7535275e240ca011bd6ccc1879
Yusuf <easyclick06@gmail.com>
2025-05-18 22:47:31 +0100

    * Remove app_url from env variables

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -28,7 +28,6 @@ func init() {
 
 	PORT, _ = strconv.Atoi(os.Getenv("PORT"))
 	ENV = os.Getenv("ENV")
-	APP_URL = os.Getenv("APP_URL")
 	DB_HOST = os.Getenv("DB_HOST")
 	DB_NAME = os.Getenv("DB_NAME")
 	DB_USER = os.Getenv("DB_USER")
