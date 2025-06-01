92382329974f15eedc31f0d0d9bedd8f8b3cab32
Yusuf Adeniyi <easyclick06@gmail.com>
2025-06-01 16:07:27 +0100

    [CU-86998pvwu] Update customer and distributor models to not reference each other but reference workspace instead (#14)

--- backend/internal/models/customer.go
+++ backend/internal/models/customer.go
@@ -4,17 +4,14 @@ import "gorm.io/gorm"
 
 type Customer struct {
 	gorm.Model
-	FirstName     string      `gorm:"not null;type:varchar(100);check:first_name <> ''" json:"firstName" validate:"required,max=100"`
-	LastName      string      `gorm:"not null;type:varchar(100);check:last_name <> ''" json:"lastName" validate:"required,max=100"`
-	PhoneNumber   string      `gorm:"not null;type:varchar(50)" json:"phoneNumber"`
-	Email         string      `gorm:"uniqueIndex;not null;type:varchar(100);check:email <> ''" json:"email" validate:"required,max=100"`
-	State         string      `gorm:"type:varchar(50)" json:"state"`
-	City          string      `gorm:"type:varchar(50)" json:"city"`
-	Country       string      `gorm:"type:varchar(50)" json:"country"`
-	Address       string      `gorm:"not null;type:varchar(200)" json:"address"`
-	WorkspaceID   uint        `json:"workspaceId"`
-	Workspace     Workspace   `gorm:"foreignKey:WorkspaceID" json:"workspace"`
-	DistributorID uint        `json:"distributorId"`
-	Distributor   Distributor `gorm:"foreignKey:DistributorID" json:"distributor"`
-	Orders        []Order     `gorm:"foreignKey:CustomerID" json:"orders"`
+	FirstName   string      `gorm:"not null;type:varchar(100);check:first_name <> ''" json:"firstName" validate:"required,max=100"`
+	LastName    string      `gorm:"not null;type:varchar(100);check:last_name <> ''" json:"lastName" validate:"required,max=100"`
+	PhoneNumber string      `gorm:"not null;type:varchar(50)" json:"phoneNumber"`
+	Email       string      `gorm:"uniqueIndex;not null;type:varchar(100);check:email <> ''" json:"email" validate:"required,max=100"`
+	State       string      `gorm:"type:varchar(50)" json:"state"`
+	City        string      `gorm:"type:varchar(50)" json:"city"`
+	Country     string      `gorm:"type:varchar(50)" json:"country"`
+	Address     string      `gorm:"not null;type:varchar(200)" json:"address"`
+	Workspace   []Workspace `gorm:"many2many:customer_workspaces;" json:"workspaces"`
+	Orders      []Order     `gorm:"foreignKey:CustomerID" json:"orders"`
 }
