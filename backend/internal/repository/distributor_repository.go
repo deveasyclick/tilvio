3032729ca4246309c11de757499822cf16eb6d67
Yusuf <easyclick06@gmail.com>
2025-06-04 17:27:14 +0100

    * Add distributor repository

---
+++ backend/internal/repository/distributor_repository.go
@@ -0,0 +1,71 @@
+package repository
+
+import (
+	"github.com/deveasyclick/tilvio/internal/models"
+	"gorm.io/gorm"
+)
+
+const (
+	whereID      = "id = ?"
+	whereEmail   = "email = ?"
+	whereClerkID = "clerk_id = ?"
+)
+
+type DistributorRepository interface {
+	Create(distributor *models.Distributor) error
+	Update(distributor *models.Distributor) error
+	DeleteByID(clerkID string) error
+	FindByID(ID string) (*models.Distributor, error)
+	FindByEmail(email string) (*models.Distributor, error)
+	FindByClerkID(clerkID string) (*models.Distributor, error)
+}
+
+type distributorRepository struct {
+	db *gorm.DB
+}
+
+func (r *distributorRepository) Create(distributor *models.Distributor) error {
+	return r.db.Create(distributor).Error
+}
+
+func (r *distributorRepository) Update(distributor *models.Distributor) error {
+	return r.db.Where(whereID, distributor.ID).Updates(distributor).Error
+}
+
+func (r *distributorRepository) DeleteByID(ID string) error {
+	return r.db.Where(whereID, ID).Delete(&models.Distributor{}).Error
+}
+
+func (r *distributorRepository) FindByID(ID string) (*models.Distributor, error) {
+	var distributor models.Distributor
+	err := r.db.Where(whereID, ID).First(&distributor).Error
+	if err != nil {
+		return nil, err
+	}
+	return &distributor, nil
+}
+
+func (r *distributorRepository) FindByEmail(email string) (*models.Distributor, error) {
+	var distributor models.Distributor
+	err := r.db.Where(whereEmail, email).First(&distributor).Error
+	if err != nil {
+		return nil, err
+	}
+	return &distributor, nil
+}
+
+func (r *distributorRepository) FindByClerkID(clerkID string) (*models.Distributor, error) {
+	var distributor models.Distributor
+	err := r.db.Where(whereClerkID, clerkID).First(&distributor).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &distributor, nil
+}
+
+func NewDistributorRepository(db *gorm.DB) DistributorRepository {
+	return &distributorRepository{db: db}
+}
