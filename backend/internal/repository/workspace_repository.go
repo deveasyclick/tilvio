6caa38da3fe5f7f51c4229ed69d85ef9199f1a5a
Yusuf <easyclick06@gmail.com>
2025-06-11 13:51:45 +0100

    * Add workspace repository

---
+++ backend/internal/repository/workspace_repository.go
@@ -0,0 +1,53 @@
+package repository
+
+import (
+	"github.com/deveasyclick/tilvio/internal/models"
+	"gorm.io/gorm"
+)
+
+type WorkspaceRepository interface {
+	Create(workspace *models.Workspace) error
+	Update(workspace *models.Workspace) error
+	Delete(ID uint) error
+	FindByID(ID uint) (*models.Workspace, error)
+	FindByDistributorID(distributorID uint) ([]models.Workspace, error)
+}
+
+type workspaceRepository struct {
+	db *gorm.DB
+}
+
+func (r *workspaceRepository) Create(workspace *models.Workspace) error {
+	return r.db.Create(workspace).Error
+}
+
+func (r *workspaceRepository) Update(workspace *models.Workspace) error {
+	return r.db.Save(workspace).Error
+}
+
+func (r *workspaceRepository) Delete(ID uint) error {
+	return r.db.Delete(&models.Workspace{}, ID).Error
+}
+
+func (r *workspaceRepository) FindByID(ID uint) (*models.Workspace, error) {
+	var workspace models.Workspace
+	err := r.db.First(&workspace, ID).Error
+	if err != nil {
+		return nil, err
+	}
+	return &workspace, nil
+}
+
+func (r *workspaceRepository) FindByDistributorID(distributorID uint) ([]models.Workspace, error) {
+	var workspaces []models.Workspace
+	err := r.db.Where("id IN (SELECT workspace_id FROM distributors WHERE id = ?)", distributorID).
+		Find(&workspaces).Error
+	if err != nil {
+		return nil, err
+	}
+	return workspaces, nil
+}
+
+func NewWorkspaceRepository(db *gorm.DB) WorkspaceRepository {
+	return &workspaceRepository{db: db}
+}
