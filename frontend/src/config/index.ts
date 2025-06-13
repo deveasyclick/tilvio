acd0e9731f2a3043e41631ad86db6a2b22e933b7
Yusuf <easyclick06@gmail.com>
2025-06-13 09:23:04 +0100

    Add base url to config file

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -3,13 +3,26 @@ type EnvVar<T> = {
   required?: boolean;
 };
 
+enum Environments {
+  DEVELOPMENT = 'development',
+  PRODUCTION = 'production',
+}
 type ENV_VARS = {
   clerkPublishableKey: EnvVar<string>;
+  environment: EnvVar<Environments>;
+  apiBaseUrl: EnvVar<string>;
 };
 
 const configs: ENV_VARS = {
   clerkPublishableKey: {
     value: import.meta.env.VITE_CLERK_PUBLISHABLE_KEY,
+    required: true,
+  },
+  environment: {
+    value: import.meta.env.MODE as Environments,
+  },
+  apiBaseUrl: {
+    value: import.meta.env.VITE_API_BASE_URL,
     required: true,
   },
 };
