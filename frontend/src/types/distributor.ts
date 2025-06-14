c650d8f4dd4a83cd21cbfa3fbf7b0e0ed88f15a4
Yusuf <easyclick06@gmail.com>
2025-06-14 11:14:47 +0100

    - Add fetch distributor query

---
+++ frontend/src/types/distributor.ts
@@ -0,0 +1,19 @@
+import type { Workspace } from './workspace';
+
+export type Role = 'distributor' | 'manager';
+
+export interface Distributor {
+  id: number;
+  createdAt: string | Date;
+  updatedAt: string | Date;
+  clerkId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone?: string;
+  state?: string;
+  city?: string;
+  address?: string;
+  role: Role;
+  workspace?: Workspace;
+}
