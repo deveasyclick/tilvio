6cfdcae078a9c54b5facdde8c44d84c3ac04d8cb
Yusuf <easyclick06@gmail.com>
2025-06-14 12:45:36 +0100

    Add handler for  fetching authenticated distributor

---
+++ backend/internal/handler/distributor_handler.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+
+	"github.com/deveasyclick/tilvio/internal/service"
+)
+
+type DistributorHandler interface {
+	GetAuthenticated(w http.ResponseWriter, r *http.Request)
+}
+
+type distributorHandler struct {
+	service service.DistributorService
+}
+
+func (h *distributorHandler) GetAuthenticated(w http.ResponseWriter, r *http.Request) {
+	distributorClerkId := r.Context().Value("userClerkId").(string)
+	distributor, err := h.service.GetDistributorByClerkID(distributorClerkId, "Workspace")
+	if err != nil {
+		slog.Error("failed to get distributor", "error", err, "clerkId", distributorClerkId)
+		http.Error(w, "failed to get distributor, try again later", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(distributor)
+}
+
+func NewDistributorHandler(service service.DistributorService) DistributorHandler {
+	return &distributorHandler{service: service}
+}
