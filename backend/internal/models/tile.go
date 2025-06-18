ba9b83ca4b7dd7d04e5b2262eeba8d3fe848985a
Yusuf <easyclick06@gmail.com>
2025-06-18 14:56:02 +0100

    Update Tile model to use pointer for manufacturer so a to prevent getting manufacturer with empty fields in filter response without preload in query

--- backend/internal/models/tile.go
+++ backend/internal/models/tile.go
@@ -6,12 +6,12 @@ import (
 
 type Tile struct {
 	gorm.Model
-	ManufacturerID uint         `json:"manufacturerId"`
-	Manufacturer   Manufacturer `gorm:"foreignKey:ManufacturerID" json:"manufacturer"`
-	Code           string       `gorm:"uniqueIndex;not null;type:varchar(100)" json:"code"`
-	Description    string       `gorm:"type:varchar(255)" json:"description"`
-	Dimension      string       `gorm:"index;not null;type:varchar(50)" json:"dimension"`
-	Type           string       `gorm:"not null;type:varchar(50)" json:"type"`
-	ImageURL       string       `gorm:"type:varchar(255)" json:"imageUrl"`
-	WeightInKg     float32      `gorm:"not null" json:"weightInKg"`
+	ManufacturerID uint          `json:"manufacturer_id"`
+	Manufacturer   *Manufacturer `gorm:"foreignKey:ManufacturerID" json:"manufacturer,omitempty"`
+	Code           string        `gorm:"uniqueIndex;not null;type:varchar(100)" json:"code"`
+	Description    string        `gorm:"type:varchar(255)" json:"description"`
+	Dimension      string        `gorm:"index;not null;type:varchar(50)" json:"dimension"`
+	Type           string        `gorm:"not null;type:varchar(50)" json:"type"`
+	ImageURL       string        `gorm:"type:varchar(255)" json:"imageUrl"`
+	WeightInKg     float32       `gorm:"not null" json:"weightInKg"`
 }
