7ae6bfa1e38c0e582cfc846463fdc0c79eebc361
Yusuf <easyclick06@gmail.com>
2025-07-03 09:36:25 +0100

    - Update Distributor and DistributorID to CreatedBy and CreatedByID - Make CreatedBy and Workspace a pointer so as not to always include them in api response - Add default `active`  value to status field

--- backend/internal/models/pricelist.go
+++ backend/internal/models/pricelist.go
@@ -6,11 +6,11 @@ import (
 
 type PriceList struct {
 	gorm.Model
-	Name           string          `gorm:"not null;type:varchar(100)" json:"name"`
-	Status         string          `gorm:"not null;type:varchar(20)" json:"status"`
-	DistributorID  uint            `json:"distributorId"` // Foreign key
-	CreatedBy      Distributor     `gorm:"foreignKey:DistributorID" json:"createdBy"`
+	Name           string          `gorm:"not null;type:varchar(100);unique" json:"name"`
+	Status         string          `gorm:"not null;type:varchar(20);default:'active'" json:"status"`
+	CreatedByID    uint            `json:"createdById"` // Foreign key
+	CreatedBy      *Distributor    `gorm:"foreignKey:CreatedByID" json:"createdBy,omitempty"`
 	WorkspaceID    uint            `json:"workspaceId"`
-	Workspace      Workspace       `gorm:"foreignKey:WorkspaceID" json:"workspace"`
+	Workspace      *Workspace      `gorm:"foreignKey:WorkspaceID" json:"workspace"`
 	PriceListItems []PriceListItem `gorm:"foreignKey:PriceListID" json:"priceListItems"`
 }
