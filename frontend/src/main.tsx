9fcbfd4e828d883c3ff154af3ed39925eea9fcab
Yusuf <easyclick06@gmail.com>
2025-07-12 21:52:16 +0100

    integrate sonner into app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import AppRoutes from './routes/index.tsx';
 import { ThemeProvider } from './contexts/theme/index.ts';
+import { Toaster } from './components/ui/sonner.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
       <AppRoutes />
+      <Toaster position="top-center" />
     </ThemeProvider>
   </StrictMode>,
 );
