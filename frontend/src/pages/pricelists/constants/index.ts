3d8413053ceab7802433719ea292fb35702ef367
Yusuf Adeniyi <easyclick06@gmail.com>
2025-07-17 09:19:16 +0100

    Fix/pricelist create or edit dialog (#67)
    * Fix default pricelist items dimension values
    
    * Fix dimension and description values showing  the first option even when another option is selected
    
    * fix: default to first price list item on new entry

--- frontend/src/pages/pricelists/constants/index.ts
+++ frontend/src/pages/pricelists/constants/index.ts
@@ -1,10 +1,10 @@
 export const DEFAULT_PRICELIST_ITEMS = [
-  { dimension: '25X40', description: 'inkjet', price: 5800 },
-  { dimension: '25X50', description: 'crack', price: 6300 },
-  { dimension: '40X40', description: 'vitrified', price: 7250 },
-  { dimension: '40X40', description: 'design', price: 7850 },
-  { dimension: '60X60', description: 'glazed', price: 7880 },
-  { dimension: '60X60', description: 'super polish', price: 10500 },
+  { dimension: '250X400', description: 'inkjet', price: 5800 },
+  { dimension: '250X500', description: 'crack', price: 6300 },
+  { dimension: '400X400', description: 'vitrified', price: 7250 },
+  { dimension: '400X400', description: 'design', price: 7850 },
+  { dimension: '600X600', description: 'glazed', price: 7880 },
+  { dimension: '600X600', description: 'super polish', price: 10500 },
 ];
 
 export const TILES_DESCRIPTIONS = [
